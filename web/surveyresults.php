ca0f4999f9607b10348e5b67f3c5ee19e4be067f
broat-sarah <bro13003@byui.edu>
2016-10-01 18:59:19 -0400

    Week 3 Survey

--- web/surveyresults.php
+++ web/surveyresults.php
@@ -1,7 +1,16 @@
+<?php
+include 'sessionstart.php';
+
+$hero = filter_input(INPUT_POST, 'hero');
+$villain = filter_input(INPUT_POST, 'villain');
+$movie = filter_input(INPUT_POST, 'movie');
+$sidekick = filter_input(INPUT_POST, 'sidekick');
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>CS 313 | Assignments</title>
+        <title>CS 313 | Survey Results</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="styles.css" type="text/css" rel="stylesheet" />
@@ -29,11 +38,12 @@
         <main>
             <div id="container" class="center">
                     <section id="content">
-                        <h1 id="h1-index">Assignments</h1>
+                        <h1 id="h1-index">Survey Results</h1>
                         <div id="text-index">
-                            <ul>
-                                <li><a href="survey.php">Week 3 - Survey</a></li>
-                            </ul>
+                            <p>You voted for <strong><?php echo $hero; ?></strong> as your favorite hero</p><br />
+                            <p>You voted for <strong><?php echo $villain; ?></strong> as your favorite villain</p><br />
+                            <p>You voted for <strong><?php echo $movie; ?></strong> as your favorite movie</p><br />
+                            <p>You voted for <strong><?php echo $sidekick; ?></strong> as your favorite sidekick</p>
                         </div>
                     </section>
                 </div>
